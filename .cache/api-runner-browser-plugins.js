766e0752f4743c38da86b063f9446c8f86366fcc
Henry Doce <henrydoce@protonmail.com>
2021-01-12 18:53:48 -0600

    It was the freaking casing of one of the image files

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/K!KO Favicons/favicon-16x16.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"c7bb6110fa9b81e6a1cd329588645669"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/KiKO Favicons/favicon-16x16.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"c7bb6110fa9b81e6a1cd329588645669"},
     },{
       plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
