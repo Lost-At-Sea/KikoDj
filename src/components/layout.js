44183414a803e17e9e8a947a57aae9563a5b6d5c
Henry Doce <henrydoce@protonmail.com>
2021-01-13 22:56:37 -0600

    Optimized images. Updated styling for tablets.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Hero from './hero';
 import SocialLinks from './socialLinks';
