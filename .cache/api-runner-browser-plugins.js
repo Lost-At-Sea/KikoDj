1a61a92abeb6f3ad4ac586f0b4a4a0852fec026e
Henry Doce <henrydoce@protonmail.com>
2021-01-14 19:38:06 -0600

    Testing a fix for bugs from running a forced npm audit (will not do that again).

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,7 @@
 module.exports = [{
-      plugin: require('/Users/henrydoce/code/KikoDj/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/KiKO Favicons/favicon-32x32.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"a773b3d569f6e1d837eeb7bf73784f86"},
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/KiKO Favicons/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"a773b3d569f6e1d837eeb7bf73784f86"},
     },{
-      plugin: require('/Users/henrydoce/code/KikoDj/gatsby-browser.js'),
+      plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/henrydoce/code/KikoDj/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/KiKO Favicons/favicon-32x32.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"a773b3d569f6e1d837eeb7bf73784f86"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","display":"minimal-ui","icon":"src/images/assets/KiKO Favicons/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"a773b3d569f6e1d837eeb7bf73784f86"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
